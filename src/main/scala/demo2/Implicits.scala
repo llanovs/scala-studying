cefc3d17455313275ea9fa9f34de86645bc97f9d
Liliia Lanovska <Liliia.Lanovska@netent.com>
2021-03-15 22:59:05 +0200

    scala Implicits

--- src/main/scala/demo2/Implicits.scala
+++ src/main/scala/demo2/Implicits.scala
@@ -1,5 +1,8 @@
 package demo2
 
+import demo2.Implicits.{Donut, DonutString}
+import DonutImplicits._
+
 object Implicits extends App {
 
   //The Implicit Modifier
@@ -35,4 +38,55 @@ object Implicits extends App {
     override def pow(x: Int, y: Int): Int = Math.round(Math.pow(x.toDouble, y.toDouble)).toInt
   }
 
+
+  //How To Create Function With Implicit Parameters
+  
+  def totalCost(donutType: String, quantity: Int)(implicit discount: Double): Double = {
+    println(s"Calculating the price for $quantity $donutType")
+    val totalCost = 2.50 * quantity * (1 - discount)
+    totalCost
+  }
+
+  implicit val discount: Double = 0.1
+  println(totalCost("Glazed Donut", 5))
+
+  //How To Create Function With Multiple Implicit Parameters
+  
+  def totalCost2(donutType: String, quantity: Int)(implicit discount: Double, storeName: String): Double = {
+    println(s"$storeName calculating the price for $quantity $donutType")
+    val totalCost = 2.50 * quantity * (1 - discount)
+    totalCost
+  }
+  
+  implicit val storeName: String = "Tasty Donut Store"
+  println(totalCost2("Glazed Donut", 5))
+  
+  //How To Create Implicit Function
+  class DonutString(s: String) {
+    def isFavoriteDonut: Boolean = s == "Glazed Donut"
+  }
+  
+  val glazedDonut = "Glazed Donut"
+  val vanillaDonut = "Vanilla Donut"
+
+  implicit def stringToDonutString(s: String) = new DonutString(s)
+  
+  println(s"Is Glazed Donut my favorive: ${glazedDonut.isFavoriteDonut}")
+  println(s"Is Vanilla Donut my favorite: ${vanillaDonut.isFavoriteDonut}")
+  
+  //How To Use Implicit Class
+
+  case class Donut(name: String, price: Double, productCode: Option[Long] = None)
+
+  val vanillaDonut2: Donut = Donut("Vanilla", 1.50)
+  
+  println(s"Vanilla donut uuid = ${vanillaDonut2.uuid}")
+  
 }
+
+object DonutImplicits {
+  implicit class AugmentedDonut(donut: Donut) {
+    def uuid: String = s"${donut.name} - ${donut.productCode.getOrElse(12345)}"
+  }
+}
+
