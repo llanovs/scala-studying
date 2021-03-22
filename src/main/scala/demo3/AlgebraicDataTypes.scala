4c58da1b3a7b6f0f16d2aa341c797599862e9d8d
Liliia <45741777+llanovs@users.noreply.github.com>
2021-03-22 20:09:02 +0200

    added ADT description from the lesson 5

---
+++ src/main/scala/demo3/AlgebraicDataTypes.scala
@@ -0,0 +1,47 @@
+package demo3
+
+object AlgebraicDataTypes extends App {
+
+  // ALGEBRAIC DATA TYPES
+
+  // Algebraic Data Types or ADTs is a commonly used way of structuring data, used in many programming
+  // languages (so this is not something unique to Scala).
+  //
+  // While the definition may sound scientific and complex, in reality we have already been using this
+  // concept in `basics` package. For example, see `sealed trait Shape` in `ClassesAndTraits`. We will now
+  // look into the concept and its use cases in more detail.
+
+  // ADTs are widely used in Scala for multiple reasons:
+  // - to ensure it is hard or even impossible to represent invalid data;
+  // - because pattern matching and ADTs play nicely together.
+
+  // Two common classes of ADTs are:
+  // 1. product types: case classes and tuples;
+  // 2. sum types: sealed traits and abstract classes.
+
+  // PRODUCT TYPES
+
+  // A product type allows to combine multiple values into one. Canonical Scala examples of product types are
+  // case classes and tuples. See `Basics` and `ClassesAndTraits` for their introduction.
+
+  // A product type is called like that because one can calculate how many different values it can possibly
+  // have by multiplying the number of such possibilities for the types it combines. The resulting number
+  // is called the arity of the product type.
+
+  // VALUE CLASSES
+
+  // Value classes are a mechanism in Scala to avoid allocating runtime objects, while still providing
+  // additional type safety. Runtime objects are not allocated in most cases, but there are notable
+  // exceptions, see the following link for more details:
+  // https://docs.scala-lang.org/overviews/core/value-classes.html
+
+  // SMART CONSTRUCTORS
+
+  // Smart constructor is a pattern, which allows creating only valid instances of a class.
+
+  // SUM TYPES
+
+  // A sum type is an enumerated type. To define it one needs to enumerate all its possible variants.
+
+  
+}
